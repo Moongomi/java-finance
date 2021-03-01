1da835b971230d4f9c34ed824993d552f4f67c43
Moongomi <kyrdudn2@naver.com>
2021-03-01 13:54:04 +0900

    init

---
+++ src/main/java/com/example/javafinance/JavaFinanceApplication.java
@@ -0,0 +1,13 @@
+package com.example.javafinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFinanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaFinanceApplication.class, args);
+    }
+
+}
